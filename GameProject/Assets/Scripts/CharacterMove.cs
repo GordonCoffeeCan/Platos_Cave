29681596f3b2bf1d0a093c56b9f04e1deb2a7b59
GordonCoffeeCan <664220842@qq.com>
2017-01-27 22:32:33 -0500

    Desktop new

---
+++ GameProject/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+    private float zDir = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0, 0, zDir) * Time.deltaTime;
+
+        if (transform.position.z > 4.8f || transform.position.z < -1) {
+            zDir *= -1;
+        }
+	}
+}
---
+++ GameProject/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+	public float speed = 5;
+	public float rotationSpeed = 8;
+
+	private Transform _mainCamera;
+	private Transform _weaponPivot;
+	private Vector3 _moveDirection;
+	private float _gravity = 20;
+	private Transform _transfrom;
+
+	private CharacterController _characterController;
+
+	// Use this for initialization
+	void Start () {
+		_mainCamera = GameObject.Find ("MainCamera").transform;
+		_transfrom = this.transform;
+		_weaponPivot = _transfrom.FindChild ("WeaponPivot");
+		_moveDirection = Vector3.zero;
+		_characterController = this.GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		Quaternion _controllerTargetRotation = Quaternion.Euler (0, _mainCamera.eulerAngles.y, 0);
+		Quaternion _pivotTargetRotation = Quaternion.Euler (_mainCamera.eulerAngles.x, _mainCamera.eulerAngles.y, 0);
+		_transfrom.rotation = Quaternion.Slerp (_transfrom.rotation, _controllerTargetRotation, rotationSpeed * Time.deltaTime);
+		_weaponPivot.rotation = Quaternion.Slerp (_weaponPivot.rotation, _pivotTargetRotation, rotationSpeed * Time.deltaTime);
+		if (_characterController.isGrounded) {
+			_moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			_moveDirection = _transfrom.TransformDirection (_moveDirection);
+			_moveDirection *= speed;
+		}
+		_moveDirection.y -= _gravity * Time.deltaTime;
+		_characterController.Move (_moveDirection * Time.deltaTime);
+		_mainCamera.position = new Vector3(_transfrom.position.x, _transfrom.position.y + 1, _transfrom.position.z);
+	}
+}
